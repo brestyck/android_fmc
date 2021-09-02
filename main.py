a1cfb9bbb0b65589bf42834bcf62f3ec905711fd
brestyck <47777597+brestyck@users.noreply.github.com>
2021-09-02 18:38:14 +0300

    Bug fixed

--- main.py
+++ main.py
@@ -17,18 +17,20 @@ print(df.colorizer("style_default"))
 print(f"{df.y()}Calculating course....{df.d()}")
 course = df.get_the_course(thc_origin, thc_destination)
 print(f"{df.g()}Course calculated!{df.d()}")
-
+landing_mode = False
 
 while True:
 
+    # Проверка наличия точек в маршруте
     if pnt != len(course) - 1:
-        current_point = course[pnt]
-        df.tts(f"NEXT POINT IS {current_point}")
-        current_point = pd.POINTS[current_point]
-        landing_mode = False
+        current_point = course[pnt]  # Take the point with NUM PNT from course
+        current_point = pd.POINTS[current_point]  # Get the coordinates
+        landing_mode = False  # Do not set the landing mode
     else:
-        current_point = thc_destination
-        df.tts(f"AIRCRAFT LANDING MODE, LANDING POINT IS {current_point}")
+        # Точек нет, посадочный режим
+        current_point = thc_destination  # Since no points, take the THC as a point
+        if not landing_mode:  # If we hadn't TTS
+            df.tts(f"AIRCRAFT LANDING MODE, LANDING POINT IS {current_point}")
         landing_mode = True
 
     if i > 20:
@@ -37,24 +39,24 @@ while True:
     df.cls()
 
     if df.toFixed(gps["latitude"], 3) == current_point[0]:
-        if df.toFixed(gps["longitude"], 3) == current_point[1]:
-            if not landing_mode:
+        if df.toFixed(gps["longitude"], 3) == current_point[1]:  # If lat and lon equal each other
+            if not landing_mode:  # Just pass the point with craft
                 print(f"APPROACHED POINT {course[pnt]}")
                 df.tts(f"APPROACHED POINT {course[pnt]}")
                 pnt += 1
                 sleep(5)
-            if landing_mode:
+            if landing_mode:  # We are on the THC, TTS for the crew to stop
                 df.tts(f"RETARD")
 
     if not landing_mode:
         up_params = f"{thc_origin} - {thc_destination} | {df.g()}{i}/20{df.d()} | POINT {df.r()}{course[pnt]}{df.d()}"
-        inf.aero_cross(up_params, gps["altitude"], gps["speed"])
+        inf.aero_cross(up_params, gps["altitude"], gps["speed"])  # Set a default cross
     else:
         up_params = f"{thc_origin} - {thc_destination} | {df.g()}{i}/20{df.d()} |" \
-                    f" {df.y()}LANDING {df.r()}{course[pnt]}{df.d()}"
-        altitude = gps["altitude"]
+                    f" {df.y()}LANDING {df.r()}{current_point}{df.d()}"  # Set the landing cross
+        altitude = gps["altitude"]  # We have to be on an estimated altitude
         altitude_est = pd.HUBS[thc_destination][2]
-        ils = pd.HUBS[thc_destination][3]
+        ils = pd.HUBS[thc_destination][3]  # Check whether we have ILS
         inf.aero_cross(up_params, f"{altitude}|^|{altitude_est}", gps["speed"], ils)
 
     sleep(1)
